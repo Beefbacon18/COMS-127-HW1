ac595fa0e1f410a87c71d48af5b5def45ee927ad
Eitan Tuchin <eitantuchin@eitant18.student.iastate.edu>
2023-06-06 15:08:28 -0500

    first commit

---
+++ zanyText.py
@@ -0,0 +1,24 @@
+# Eitan Tuchin            09/07/22
+# Assignment 1
+
+print("Zany Text!")
+print()
+print("By: Eitan Tuchin")
+print("[COM S 127 [Section B]]")
+print()
+j = 0
+
+while j < 3:
+        noun1 = input("Enter a noun: ")
+        adjective1 = input("Enter an adjective: ")
+        adjective2 = input("Enter an adjective: ")
+        adjective3 = input("Enter an adjective: ")
+        verb1 = input("Enter a past tense action: ")
+        noun2 = input("Enter a noun: ")
+        print("Once upon a time, there was a {0}. It was a {1}, {2}, {3}, {4}. And, one day it {5} all over the {6}!".format(noun1,
+        adjective1, adjective2, adjective3, noun1, verb1, noun2))
+        j += 1
+
+print("ASSIGNMENT 1 REVISIONED")
+
+
